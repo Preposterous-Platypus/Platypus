d2ffabbb2257147acf93740a576992ac6fa75765
Arun Vasudevan <arunv83@gmail.com>
2016-04-08 11:23:54 -0700

    Add createOne method for adding a new user

--- server/users/userController.js
+++ server/users/userController.js
@@ -2,4 +2,13 @@ var User = require('./userModel.js');
 
 module.exports = {
   //all methods - createOne, delete, removeOne
+  createOne: function(req, res) {
+    var newUser = req.body;
+    User.create(newUser, function(err, data) {
+      if (err) {
+        return res.json(err);
+      }
+      res.json(data);
+    });
+  },
 };
