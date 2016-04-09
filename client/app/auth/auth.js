8360e1996bfe8d74e3d95fdb783153c8d1fcaa20
Kim Curran <currankim7@gmail.com>
2016-04-08 19:29:55 -0700

    Begin implementing Github authentication for login

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,15 +1,3 @@
 angular.module('platypus.auth', [])
 .controller('AuthController', function($scope){
-  $scope.user = {};
-
-  $scope.signin = function () {
-    Auth.signin()
-      .then(function (token) {
-        $window.localStorage.setItem('com.platypus', token);
-        $location.path('/signin');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
 });
