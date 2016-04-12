94d63d58396c72a79ffc4f177fcc68d0835a646a
Claire Hsu <clairehsu86@gmail.com>
2016-04-12 10:45:10 -0700

    rename liked likes, built likesController

---
+++ server/likes/likesController.js
@@ -0,0 +1,44 @@
+var Likes = require('../likesModel.js');
+
+module.exports = {
+  addOne: function(req, res) {
+  var newLikes = req.body;
+  Likes.create(newLikes, function(err, response) {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(response);
+  });
+},
+
+retrieveAll: function(req, res) {
+  var query = req.query;
+  Likes.find(query, function(err, response) {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(response);
+  });
+},
+
+retrieveOne: function(req, res) {
+  var query = {_id: req.params.id};
+  Likes.findOne(query, function(err, response) {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(response);
+  });
+},
+
+removeOne: function(req, res) {
+  var query = {_id: req.params.id};
+  Likes.findOneAndRemove(query, function(err, response) {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(response);
+  });
+  }
+
+};
