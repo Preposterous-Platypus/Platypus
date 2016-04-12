6237d6416d499fe64573472606b8242f72c3a6bb
Claire Hsu <clairehsu86@gmail.com>
2016-04-12 09:30:32 -0700

    updated likescontroller

--- server/liked/likesController.js
+++ server/liked/likesController.js
@@ -31,22 +31,7 @@ retrieveOne: function(req, res) {
   });
 },
 
-deleteAll: function(req, res) {
-  var query = req.query;
-  Likes.find(query, function(err, data) {
-    if (err) {
-      return res.json(err);
-    }
-    Likes.remove(query, function(err, data) {
-      if (err) {
-        return res.json(err);
-      }
-      res.json(data);
-    });
-  });
-},
-
-deleteOne: function(req, res) {
+removeOne: function(req, res) {
   var query = {_id: req.params.id};
   Likes.findOneAndRemove(query, function(err, data) {
     if (err) {
