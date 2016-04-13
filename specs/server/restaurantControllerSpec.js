bc669ba0a29069f1961aaeee3d30cf769156e2ad
Claire Hsu <clairehsu86@gmail.com>
2016-04-13 12:12:08 -0700

    started building out tests for restaurantController

--- specs/server/restaurantControllerSpec.js
+++ specs/server/restaurantControllerSpec.js
@@ -27,66 +27,70 @@ describe('Restaurant Controller', function () {
       var restaurants = [
         {
           name: 'ChickenWaffle',
-          yelpID: 'asdfasdfasdf',
-          distance: String,
-          image_url: String,
-          likes: Number
+          yelpID: 'ChickenWaffleID',
+          url: 'www.chickenwaffle.com',
+          eat24_url: 'www.eat24chickenwaffle.com',
+          image_url: 'ChickenWaffleURL',
+          likes: 3
         },
         {
-          name: {
-            type: String,
-            unique: true,
-            required: true
-          },
-          yelpID: String,
-          distance: String,
-          image_url: String,
-          likes: Number
+          name: 'SoyKingdom',
+          yelpID: 'SoyKingdomID',
+          url: 'www.SoyKingdom.com',
+          eat24_url: 'www.eat24SoyKingdom.com',
+          image_url: 'SoyKingdomURL',
+          likes: 3
         },
         {
-           name: {
-            type: String,
-            unique: true,
-            required: true
-          },
-          yelpID: String,
-          distance: String,
-          image_url: String,
-          likes: Number
+          name: 'BurgerHeaven',
+          yelpID: 'BurgerHeavenID',
+          url: 'www.BurgerHeaven.com',
+          eat24_url: 'www.eat24BurgerHeaven.com',
+          image_url: 'BurgerHeavenURL',
+          likes: 3
         },
         {
-          name: {
-            type: String,
-            unique: true,
-            required: true
-          },
-          yelpID: String,
-          distance: String,
-          image_url: String,
-          likes: Number
+          name: 'GravyTrain',
+          yelpID: 'GravyTrainID',
+          url: 'www.GravyTrain.com',
+          eat24_url: 'www.eat24GravyTrain.com',
+          image_url: 'GravyTrainURL',
+          likes: 3
         }
       ];
-      Likes.create(likes, done);
+      Restaurant.create(restaurants, done);
     });
   });
 
   it('Creates likes', function (done) {
-    var newLike = {
-      restaurant: 5,
-      user: 5
+    var newRestaurant = {
+      name: 'LiteDelights',
+      yelpID: 'LiteDelightsID',
+      url: 'www.LiteDelights.com',
+      eat24_url: 'www.eat24LiteDelights.com',
+      image_url: 'LiteDelightsURL',
+      likes: 3
     };
 
-    likesController.addOne(newLike, function() {
-      Likes.findOne({restaurant: 5}, function(err, result) {
+    likesController.addOne(newRestaurant, function() {
+      Likes.findOne({name: 'LiteDelights'}, function(err, result) {
         //changed 'result' from example to 'output' so 'result' is used once
         var output = {
-          restaurant: result.restaurant,
-          user: result.user
+          name: result.name,
+          yelpID: result.yelpID,
+          url: result.url,
+          eat24_url: result.eat24_url,
+          image_url: result.image_url,
+          likes: result.likes
         };
 
         var test = {
-          restaurant: newLike.restaurant,
-          user: newLike.user
+          name: newRestaurant.name,
+          yelpID: newRestaurant.yelpID,
+          url: newRestaurant.url,
+          eat24_url: newRestaurant.eat24_url,
+          image_url: newRestaurant.image_url,
+          likes: newRestaurant.likes
         };
         expect(output).to.eql(test);
         done();
@@ -94,29 +98,62 @@ describe('Restaurant Controller', function () {
     });
   });
 
-  it('should have a method that returns all likes', function (done) {
-    likesController.retrieveAll(function(likes) {
-      expect(likes.length).to.equal(5);
-      done();
+  //updateOne
+    //not so sure about this test, as updateOne makes us of req.params.id
+  //should we use findOne to obtain the id?
+  it('should update the restaurant\'s info with what\'s given', function(done) {
+    var newURL = 'newURL.com';
+ 
+    var result = restaurantController.updateOne('_id', newURL, function() {
+      restaurantController.retrieveOne('_id', function(restaurant) {
+        result = restaurant.url;
+ 
+        expect(result).to.equal(newURL);
+        done();
+      });
     });
   });
-
-//not so sure about this test, as retrieveOne makes us of req.params.id
-//should we use findOne to obtain the id?
-//   it('should have a method that returns a like', function (done) {
-//   likesController.retrieveOne({restaurant:5}, function(likes) {
-//     expect(likes.restaurant).to.equal(5);
-//     done();
-//   });
-// });
-
-//not so sure about this test, as removeOne makes us of req.params.id
-//should we use findOne to obtain the id?
+ 
+  //removeOne
+  //not so sure about this test, as removeOne makes us of req.params.id
+  //should we use findOne to obtain the id?
   // it('should have a method that removes a like', function (done) {
   //   likesController.removeOne({restaurant:5}, function(likes) {
   //     expect(Likes.retrieveOne(5)).to.equal(undefined);
   //     done();
   //   });
   // });
+  it('should remove a restaurant from the database', function(done) {
 
+    //set length
+    var oldDB = restaurants.length;
+
+    restaurantController.removeOne('_id');
+    expect(restaurant.length).to.eql(oldDB - 1);
+    done();
+  });
+
+  //retrieveOne
+  //not so sure about this test, as retrieveOne makes us of req.params.id
+  //should we use findOne to obtain the id?
+  //   it('should have a method that returns a like', function (done) {
+  //   likesController.retrieveOne({restaurant:5}, function(likes) {
+  //     expect(likes.restaurant).to.equal(5);
+  //     done();
+  //   });
+  // });
+  it('should return the specific restaurant that was asked for', function(done) {
+    restaurantController.retrieveOne('_id', function(result) {
+    //need to complete
+    expect(result).to.eql();
+    done();
+    });
+  });
+
+  it('should have a method that returns all restaurants', function (done) {
+    restaurantController.retrieveAll(function(restaurants) {
+      expect(restaurants.length).to.equal(5);
+      done();
+    });
+  });
 });
