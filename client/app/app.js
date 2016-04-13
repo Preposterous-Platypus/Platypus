9a348f9ae80462e682971c452b01e718c7422eab
Arun Vasudevan <arunv83@gmail.com>
2016-04-13 09:36:56 -0700

    Add routes and templates to signin, feed, dashboard, and settings

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,7 @@
 angular.module('preposterous-platypus', [
   'ngRoute',
-  'platypus-auth'
+  'platypus.auth',
+  'platypus.services'
   ])
 
 .config(function ($routeProvider, $httpProvider) {
@@ -9,4 +10,23 @@ angular.module('preposterous-platypus', [
       templateUrl: 'app/auth/signin.html',
       controller: 'AuthController'
     })
+    .when('/add', {
+      templateUrl: 'app/add/add.html',
+      controller: 'AddController'
+    })
+    .when('/feed', {
+      templateUrl: 'app/feed/feed.html',
+      controller: 'FeedController'
+    })
+    .when('/user/dashboard', {
+      templateUrl: 'app/user/dashboard.html',
+      controller: 'UserController'
+    })
+    .when('/user/settings', {
+      templateUrl: 'app/user/settings.html',
+      controller: 'UserController'
+    })
+    .otherwise({
+      redirectTo: 'app/auth/signin.html'
+    });
 });
