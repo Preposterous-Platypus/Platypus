597f5ed660cc532be4a4a8a4addb60c2b2fddf9a
Claire Hsu <clairehsu86@gmail.com>
2016-04-13 17:45:02 -0700

    create add controller, specs and template

---
+++ client/app/channels/food/add/add.js
@@ -0,0 +1,14 @@
+angular.module('platypus.food-add', ['platypus.services'])
+ .controller('FoodAddController', function($scope, Restaurants){
+
+  $scope.data = {};
+  //we probably want to create a new restaurant service that fetches data from the yelp api, to use here
+  	Restaurants.getAll()
+  	.then(function(restaurants){
+  		$scope.data.restaurants = restaurants;
+
+  	}).catch(function(error){
+  		console.error(error);
+  	});
+   
+ }); 
