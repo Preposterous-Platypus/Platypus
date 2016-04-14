7e9b186250e4d280b8dc73b1e3c1e58473229d2b
Arun Vasudevan <arunv83@gmail.com>
2016-04-13 20:29:08 -0700

    Update feed.js with getAll function to be used with controller

--- client/app/channels/food/feed/feed.js
+++ client/app/channels/food/feed/feed.js
@@ -1,4 +1,14 @@
 angular.module('platypus.food-feed', [])
 .controller('FoodFeedController', function($scope) {
-  //add code here
+  
+  $scope.data = {};
+
+  //get all restaurants from database
+  Restaurant.getAll()
+  .then(function(restaurants) {
+    $scope.data.restaurants = restaurants;
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
 });
