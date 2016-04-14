0ae4838b969abe968550fe430a622a00a5e6cbc4
Arun Vasudevan <arunv83@gmail.com>
2016-04-13 19:46:44 -0700

    Add signin and signup functions to AuthController in auth.js

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,4 +1,26 @@
 angular.module('platypus.auth', [])
 .controller('AuthController', function($scope){
-  //
+  $scope.user = {};
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.platypus', token);
+        $location.path('/signin');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.platypus', token);
+        $location.path('/signup');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 });
