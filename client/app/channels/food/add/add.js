f122349d4a204b1358d2b4331cf037443c6f73f2
Arun Vasudevan <arunv83@gmail.com>
2016-04-13 20:29:40 -0700

    Update add.js to include addOne function to add restaurant to database

--- client/app/channels/food/add/add.js
+++ client/app/channels/food/add/add.js
@@ -2,6 +2,8 @@ angular.module('platypus.food-add', ['platypus.services'])
  .controller('FoodAddController', function($scope, Restaurants){
 
   $scope.data = {};
+
+  $scope.restaurant = {};
   //we probably want to create a new restaurant service that fetches data from the yelp api, to use here
   	Restaurants.getAll()
   	.then(function(restaurants){
@@ -10,5 +12,7 @@ angular.module('platypus.food-add', ['platypus.services'])
   	}).catch(function(error){
   		console.error(error);
   	});
-   
+
+    //add a restaurant
+    Restaurants.addOne($scope.restaurant);
  });
