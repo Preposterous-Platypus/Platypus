6466b6ee197dfa52bed08d060de5ed9d657e7011
Claire Hsu <clairehsu86@gmail.com>
2016-04-15 16:27:57 -0700

    Update server.js
    removed extraneous routes require, kept routes require at bottom

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ mongoose.connect('mongodb://localhost/platypus');
 
 //configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
-require('./config/routes.js')(app, express);
 
 
 
