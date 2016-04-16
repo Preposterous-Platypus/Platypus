da5b3a2f25169acbfde8f98eb8a9b4d55ff5ec91
Reid <jreid.greer@gmail.com>
2016-04-16 12:59:05 -0700

    Place passport setup code into a new passportConfig file

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
-var User = require('./users/userModel.js');
 
 var app = express();
 
@@ -19,48 +17,8 @@ app.listen(port, function(err) {
     return console.log(err);
   }
   console.log('Platypus is listening on ' + port);
-});  
-
-app.use(session({
-  secret: 'asdf',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { path: '/', httpOnly: true, secure: false, maxAge: 60000 }
-  }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new GitHubStrategy({
-  clientID: '4cc724b73df7e764536f',
-  clientSecret: 'dad7b3b87f7478e53ed79186e41ef7aea161ba15',
-  callbackURL: 'http://127.0.0.1:8000/github/callback'
-}, function(accessToken, refreshToken, profile, callback) {
-  // console.log('PROFILE >>>>>> ', profile);
-  User
-    .findOne({'gitHubHandle': profile.username}, function(err, found) {
-      if (found) {
-      	callback(null, found);
-      } else {
-        User.create({ 
-          name: profile.displayName === null ? profile.username : profile.displayName,
-          gitHubHandle: profile.username,
-          email: profile._json.email,
-          image: profile.photos[0].value
-        }, function(err, newUser) {
-          console.log('user created ', newUser);
-      	  callback(null, newUser);
-      	});
-      }
-    })
-}));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.get('id'));
 });
 
-passport.deserializeUser(function(id, done) {
-  done(null, id);
-});
-
+// Passport Configuration
+require('./config/passportConfig.js')(app, session, passport);  
 require('./config/routes.js')(app, express, passport);
