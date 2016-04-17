fa233148184350d3faa84fceb18a5e376916a31d
Arun Vasudevan <arunv83@gmail.com>
2016-04-16 19:52:25 -0700

    Update dashboard.html and user.js

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -7,11 +7,14 @@ angular.module('platypus.user', [])
     $scope.data.restaurants = {};
 
     Likes.retrieveLikedRestaurants($scope.user, function(restaurants) {
+      console.log('I\'M RIGHT HERE!!');
       $scope.data.restaurants = restaurants;
       console.log(restaurants);
       $scope.filter = '';
     });
   };
+
+  $scope.populate();
 
   //filter out restaurants
   // $scope.filter = function() {
