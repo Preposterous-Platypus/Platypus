62b457e0b8dc8a7f0babd4ecafa8a357c4171d1c
Arun Vasudevan <arunv83@gmail.com>
2016-04-18 15:40:28 -0700

    Update styling

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -1,10 +1,41 @@
 angular.module('platypus.user', [])
 
-.controller('UserController', function($scope, Likes){
+.controller('UserController', function($scope, Likes, Restaurants){
+  var updateUserLikes = function() {
+    Likes.findUserLikes()
+    .then(function(likes) {
+      $scope.data.likes = {};
+      for (var i = 0; i < likes.length; i++) {
+        $scope.data.likes[likes[i].restaurant] = true;
+      }
+    });
+  };
+
+  updateUserLikes();
+
+  //update Likes
+  $scope.updateLikes = function(restID) {
+    Likes.addOrRemove(restID)
+    .then(function(liked) {
+      Restaurants.updateLikes(restID)
+      .then(function(likes) {
+        var restaurants = $scope.data.restaurants;
+        for (var i = 0; i < restaurants.length; i++) {
+          if (restaurants[i]._id === restID) {
+            restaurants[i].likes = likes;
+          }
+        }
+        updateUserLikes();
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  };
+
   $scope.data = {};
 
   $scope.populate = function() {
-
     Likes.retrieveLikedRestaurants(function(restaurants) {
       $scope.data.restaurants = restaurants;
       console.log('RESTAURANTS: ', restaurants);
@@ -14,4 +45,6 @@ angular.module('platypus.user', [])
   $scope.populate();
 
   $scope.feedOrder = '-likes';
+
+
 });
