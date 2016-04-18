ccf00a3b6b0cc4c6ebb3753ca005801ea42bdf40
Arun Vasudevan <arunv83@gmail.com>
2016-04-18 16:19:14 -0700

    Update Dashboard to work with liked restaurants

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -1,31 +1,13 @@
 angular.module('platypus.user', [])
 
 .controller('UserController', function($scope, Likes, Restaurants){
-  var updateUserLikes = function() {
-    Likes.findUserLikes()
-    .then(function(likes) {
-      $scope.data.likes = {};
-      for (var i = 0; i < likes.length; i++) {
-        $scope.data.likes[likes[i].restaurant] = true;
-      }
-    });
-  };
-
-  updateUserLikes();
-
   //update Likes
   $scope.updateLikes = function(restID) {
     Likes.addOrRemove(restID)
     .then(function(liked) {
       Restaurants.updateLikes(restID)
       .then(function(likes) {
-        var restaurants = $scope.data.restaurants;
-        for (var i = 0; i < restaurants.length; i++) {
-          if (restaurants[i]._id === restID) {
-            restaurants[i].likes = likes;
-          }
-        }
-        updateUserLikes();
+        $scope.populate();
       });
     })
     .catch(function(error) {
@@ -37,6 +19,7 @@ angular.module('platypus.user', [])
 
   $scope.populate = function() {
     Likes.retrieveLikedRestaurants(function(restaurants) {
+      $scope.data.restaurants = [];
       $scope.data.restaurants = restaurants;
       console.log('RESTAURANTS: ', restaurants);
     });
